526358af041a7e45e8f69e6e775af381df6497d1
Harris <harriskarume.hk@gmail.com>
2022-06-02 17:40:30 +0300

    Design admin component and update routes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AddproductsComponent } from './components/addproducts/addproducts.component';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { ShowproductsComponent } from './components/showproducts/showproducts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AdminComponent, children: [
+    {path: 'adduser', component: AdduserComponent},
+    {path: 'addproduct', component: AddproductsComponent},
+    {path: 'showproducts', component: ShowproductsComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin/admin.component';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { AddproductsComponent } from './components/addproducts/addproducts.component';
+import { ShowproductsComponent } from './components/showproducts/showproducts.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent,
+    AdduserComponent,
+    AddproductsComponent,
+    ShowproductsComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
